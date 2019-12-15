5d03f3122ba6b3d3ede8a70fe2997bbe55e83938
Shadowsnhd <32468690+Shadowsnhd@users.noreply.github.com>
2019-12-14 20:07:54 -0500

    pornhub

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ function googleAnalyticsMiddleware(data) {
 }
 
 var unblockerConfig = {
-    prefix: '/textbooks/',
+    prefix: '/text/',
     responseMiddleware: [
         googleAnalyticsMiddleware
     ]
